a999d13114c3ea26c64f894d837dfdd31be8c590
santiagoroncancio <brayan.roncancio@unillanos.edu.co>
2020-05-09 11:33:36 -0500

    Implementacion de slug

--- storeofllano/products/urls.py
+++ storeofllano/products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('<pk>', views.ProductDetailView.as_view(),name='product')
+    path('<slug:slug>', views.ProductDetailView.as_view(),name='product')
 ]
