d2eb3f72792b7900d35b758404b2f63426cb2708
santiagoroncancio <brayan.roncancio@unillanos.edu.co>
2020-05-10 21:52:23 -0500

    Implementacion de un buscador de productos

--- storeofllano/products/views.py
+++ storeofllano/products/views.py
@@ -16,3 +16,18 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product.html'
+
+class ProductSearchListView(ListView):
+    template_name = 'products/search.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(title__icontains=self.query())
+
+    def query(self):
+        return self.request.GET.get('q')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        context['count'] = context['product_list'].count()
+        return context
