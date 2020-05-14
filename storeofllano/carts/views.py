ce00f20b73fe80e5fe7be1e52007f06cdbcdb75f
santiagoroncancio <brayan.roncancio@unillanos.edu.co>
2020-05-14 07:27:13 -0500

    Se incluyo el calcular el total

--- storeofllano/carts/views.py
+++ storeofllano/carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import Cart
 from .utils import get_or_create_cart
 from products.models import Product
@@ -12,7 +12,7 @@ def cart(request):
 
 def add(request):
     cart = get_or_create_cart(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
+    product = get_object_or_404(Product, pk = request.POST.get('product_id'))  
     
     cart.products.add(product)
 
@@ -21,8 +21,8 @@ def add(request):
     })
 
 def remove(request):
-    cart = get_or_create_cart(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
+    cart = get_or_create_cart(request)   
+    product = get_object_or_404(Product, pk = request.POST.get('product_id'))    
 
     cart.products.remove(product)
 
